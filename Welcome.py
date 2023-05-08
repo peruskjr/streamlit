7d41960fb8543fb030308d61b5c96f96c913ef68
peruskjr <peruskjr@gmail.com>
2023-05-08 16:36:17 -0400

    Updated the landing page

--- Welcome.py
+++ Welcome.py
@@ -1,12 +1,30 @@
 import streamlit as st
+from streamlit.logger import get_logger
 
-st.markdown("# Main page 🎈")
-st.divider()
-st.sidebar.markdown("# Main page 🎈")
-st.header("What is even happening?")
-st.divider()
-st.title("This is my app page biiiiiiiitch!")
-st.divider()
+LOGGER = get_logger(__name__)
+
+
+def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    st.write("# Welcome to my Streamlit Dashboard! 👋")
+
+    st.sidebar.success("Select an ml app above.")
+
+    st.markdown(
+        """
+        If you've made it this far checking out my stuff, I greatly appreciate it!
+        **👈 Select one of my ml apps from the sidebar** to see some examples
+        of what I've been tinkering with using Streamlit!
+    """
+    )
+
+
+if __name__ == "__main__":
+    run()
 
 
 
